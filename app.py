a2b3ad38b2250de10031c6943c58e15702f8d17b
Jin-S-L <86580553+Jin-S-L@users.noreply.github.com>
2021-07-14 00:57:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
-import numpy as np
 import datetime as dt
 import plotly.express as px 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import streamlit as st
+import requests
+import simplejson as json
 
 API_key = 'BGNKZDPGRQ7RCZ7L'
-def get_data(symbol,end_date,start_date):
-    ts = TimeSeries(key=API_key,output_format='pandas')
-    data=ts.get_monthly_adjusted(symbol)[0]
+
+def get_data(symbol,start_date,end_date):
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={symbol}&apikey=GNKZDPGRQ7RCZ7L'
+    r = requests.get(url)
+    data = r.json()
+    data=data['Monthly Adjusted Time Series']
+    data=pd.read_json(json.dumps(data))
+    data=data.transpose()
     filtered_data = data.loc[end_date:start_date]
     return filtered_data
 
@@ -25,11 +31,11 @@ end_date = st.sidebar.text_input("End Date","2020-01-01")
 
 st.header(f"{ticker} : Open, Close, Adjusted Close, High, Low Prices" )
 
-df=get_data(ticker,end_date,start_date)
+df=get_data(ticker,start_date,end_date)
 df=df.reset_index()
-df=df.sort_values(by='date')
-df=df.rename(columns={'1. open':'Open','2. high':'High','3. low':'Low','4. close':'Close','5. adjusted close':'Adjusted Close'})
+df=df.sort_values(by='index')
+df=df.rename(columns={'index':'Date','1. open':'Open','2. high':'High','3. low':'Low','4. close':'Close','5. adjusted close':'Adjusted Close'})
 
-fig=px.line(df,x='date',y=df.columns[1:6])
+fig=px.line(df,x='Date',y=df.columns[1:6])
 
-st.plotly_chart(fig, use_container_width=True)
+#st.plotly_chart(fig, use_container_width=True)
